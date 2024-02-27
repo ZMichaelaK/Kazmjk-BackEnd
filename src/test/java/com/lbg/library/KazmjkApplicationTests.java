29005ecc872c4ac3b179cc2531f6e66c528fa7c3
andyking <andrewk640@gmail.com>
2024-02-27 09:49:47 +0000

    init commit

---
+++ src/test/java/com/lbg/library/KazmjkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbg.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KazmjkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
